6f8145fd2dbf97cf93f629ec6654313fb7e0edf8
Rahul-2006 <87422105+Rahul-2006@users.noreply.github.com>
2024-09-10 10:41:12 +0530

    Updated the wording (#16)
    * Updated the wording
    
    Changed the wording of verification if you are a nad
    
    * Fix grammar + improve wording
    
    ---------
    
    Co-authored-by: port <108868128+portdeveloper@users.noreply.github.com>

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -53,7 +53,7 @@ export default function HomeComponent({ session, userIsPartOfMonad, userGuildMem
         </>
       ) : (
         <>
-          <button onClick={() => signIn()}>Let&apos;s verify if you&apos;re a Nad</button>
+          <button onClick={() => signIn()}>Check your access level</button>
         </>
       )}
     </div>
